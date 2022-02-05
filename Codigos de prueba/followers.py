d7abc8e0eda26ff26005d2830f447d4118dc4468
negroletti <negroletti@gmail.com>
2022-02-05 00:48:01 -0300

    Nueva wea

--- Codigos de prueba/followers.py
+++ Codigos de prueba/followers.py
@@ -12,37 +12,40 @@ auth.set_access_token(access_token,access_secret)
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 x = set()
-y = set()
-z = set()
+# y = set()
+# z = set()
 
+#TODO Buscar municipalidades de concepción, municipalidad de punta arenas y en stgo agregar otras municipalidades.
+#TODO Revisar capturar datos desde facebook(API DE FACEBOOK REVISAR DOCUMENTACION, QUE DATOS, PERMISOS, DIFICULTADES).
+#TODO BUSCAR MODELOS PRE ENTRANDOS PARA ANÁLISIS DE SENTIMIENTOS EN ESPAÑOL. ETIQUETAR DATOS Y ENTRENAR UN MODELO PROPIO.(SI ES QUE DA TIEMPO).
 def searchFollowers():
-    for  user in tweepy.Cursor(api.friends, screen_name="UTalca").items(100):
+    for  user in tweepy.Cursor(api.friends, screen_name="Muni_Concepcion").items(1000):
         x.add(user._json['screen_name'])
-    print ("1 listo")
-    f_out = open("Talca1.txt", "w")
+    print ("listo")
+    f_out = open("Muni_Concepcion.txt", "w")
     for i in x:
         f_out.write(i + "\n")
-    for user in tweepy.Cursor(api.friends, screen_name="RADIOPALOMAFM").items(100):
-        y.add(user._json['screen_name'])
-    print ("2 listo")
-    f_out = open("Talca2.txt", "w")
-    for i in y:
-        f_out.write(i + "\n")
-    for user in tweepy.Cursor(api.friends, screen_name="IMTalca").items(100):
-        z.add(user._json['screen_name'])
-    print ("3 listo")
-    f_out = open("Talca3.txt", "w")
-    for i in z:
-        f_out.write(i + "\n")
+    # for user in tweepy.Cursor(api.friends, screen_name="RADIOPALOMAFM").items(100):
+    #     y.add(user._json['screen_name'])
+    # print ("2 listo")
+    # f_out = open("Talca2.txt", "w")
+    # for i in y:
+    #     f_out.write(i + "\n")
+    # for user in tweepy.Cursor(api.friends, screen_name="IMTalca").items(100):
+    #     z.add(user._json['screen_name'])
+    # print ("3 listo")
+    # f_out = open("Talca3.txt", "w")7/*89
+    # for i in z:
+    #     f_out.write(i + "\n")
 
-    j= x.intersection(y)
-    print(j)
-    k= j.intersection(z)
-    print(k)
+    # j= x.intersection(y)
+    # print(j)
+    # k= j.intersection(z)
+    # print(k)
 
-    f_out = open("TalcaF.txt", "w")
-    for i in k:
-        f_out.write(i + "\n")
+    # f_out = open("TalcaF.txt", "w")
+    # for i in k:
+    #     f_out.write(i + "\n")
 
 
 if __name__ == "__main__":
